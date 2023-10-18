dcdac17f70a28b8d670bee2e18e57200cbbd7958
wiznikvibe <nikhilshetty439@gmail.com>
2023-10-18 10:50:29 +0530

    Project Structure Implemented

---
+++ template.py
@@ -0,0 +1,49 @@
+import os, sys 
+from pathlib import Path 
+import logging 
+
+while True:
+    project_name = input("Enter your Project Name: ")
+    if project_name != "":
+        break
+
+
+resources = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/exception.py",
+    f"{project_name}/logger.py",
+    f"{project_name}/utils.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_evaluation.py"
+    f"{project_name}/components/data_transformation.py"
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/pipeline/prediction_pipeline.py",
+    "app.py",
+    "setup.py"
+
+]
+
+for resource in resources:
+    file_path = Path(resource)
+    filedir, filename = os.path.split(resource)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating Directory {filedir} / file: {filename}")
+    
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path, "w") as f:
+            pass 
+
+    else: 
+        logging.info(f"File Already exists at: {filepath}")
+
+
